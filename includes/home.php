40a1b243c7c2189bf4ca8fdb7d2dccbe5992bf2b
Tiago Link <tlink.k@gmail.com>
2012-09-10 23:54:51 -0300

    alteracoes

--- includes/home.php
+++ includes/home.php
@@ -1,2 +1,11 @@
 <h2>Página Home</h2>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+
+
+<?php
+  $var1 = "3 porquinhos";
+  $var2 = (int) $var1;
+  // echo $var2 + 2;
+  echo $data = date("d/m/Y");
+
+?>
