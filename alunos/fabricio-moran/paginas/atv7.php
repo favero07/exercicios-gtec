3ffeec7d85604213e5094762a1ff6f091c4568d6
Tiago Link <tlink.k@gmail.com>
2012-09-10 23:43:27 -0300

    add arquivos Fabricio Moran

---
+++ alunos/fabricio-moran/paginas/atv7.php
@@ -0,0 +1,9 @@
+<form action="paginas/comple_atv7.php" method="post" target="_new">
+	*Nome da Dupla:<br /> <input type="text" name="ndupla" /><br />
+    *Nome do Player1:<br /> <input type="text" name="p1" /><br />
+    *Nome do Player2:<br /> <input type="text" name="p2" /><br />
+    *Email: <br /><input type="text" name="email" /><br />
+    *Ano de Criação:<br /> <input type="text" name="data" maxlength="4" /><br />
+    *Nome das 5 primeiras músicas: <br /><textarea name="musicas" ></textarea><br />
+    <input type="submit" name="cadastrar" />
+</form>
